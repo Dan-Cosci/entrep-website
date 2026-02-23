3305493da8399979a247c6b60d80b699ff549b7f
Daniel <allinasdaniel@gmail.com>
2026-02-23 09:26:09 +0800

    css setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {Toaster} from 'react-hot-toast'
+import App from './App.jsx'
+import './assets/css/main.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Toaster />
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
