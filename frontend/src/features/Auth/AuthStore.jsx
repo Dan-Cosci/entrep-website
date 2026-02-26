4f209070bc910c7e3096ff64c65f8880cf5d11c5
Daniel <allinasdaniel@gmail.com>
2026-02-26 19:26:21 +0800

    starting auth design

---
+++ frontend/src/features/Auth/AuthStore.jsx
@@ -0,0 +1,9 @@
+import create from 'zustand'
+
+const useAuthStore = create((set) => ({
+  isLogged: false,
+  login: () => set({ isLogged: true }),
+  logout: () => set({ isLogged: false }),
+}))
+
+export default useAuthStore;
