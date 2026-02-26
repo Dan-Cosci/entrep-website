17823ac49124584048a6ca81a0917546c6cf900a
Daniel <allinasdaniel@gmail.com>
2026-02-26 21:56:53 +0800

    done framework sign up and sign in

--- frontend/src/features/Auth/pages/SignIn.jsx
+++ frontend/src/features/Auth/pages/SignIn.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthInput from '../components/AuthInput'
+import AuthButton from '../components/AuthButton'
+
+import './SignIn.css'
 
 const SignIn = () => {
+
+  const [email,setEmail] = useState('');
+  const [password,setPassword]= useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email,password);
+  }
+
   return (
-    <div>SignIn</div>
+    <div className="SignIn-card">
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <AuthInput type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <AuthInput type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <AuthButton type="submit">Sign Up</AuthButton>
+      </form>
+    </div>
   )
 }
 
