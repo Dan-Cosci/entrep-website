5651b371ec83dfcb50954e704019e6f83c685f2c
Daniel <allinasdaniel@gmail.com>
2026-02-26 11:05:00 +0800

    firebase setup

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 import config from './config';
@@ -14,4 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
