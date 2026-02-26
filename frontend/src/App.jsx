1d44d98bfe492920fd17c840e14e2195922b3a77
Daniel <allinasdaniel@gmail.com>
2026-02-26 15:31:27 +0800

    shared pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Loading from './shared/pages/Loading'
+import Page404 from './shared/pages/Page404'
+import Page500 from './shared/pages/Page500'
 
 const App = () => {
   return (
     <>
-      <div className="app">Hello world</div>
+      <h1>this is the base app</h1>
+      <Loading />
+      <Page404 />
+      <Page500 />
     </>
   )
 }
