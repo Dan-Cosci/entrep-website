65399f3d5e76615c7068df0403e6fd4edd24793b
Daniel <allinasdaniel@gmail.com>
2026-02-26 15:59:03 +0800

    shared pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import {Toaster} from 'react-hot-toast'
 import App from './App.jsx'
 import './assets/css/main.css'
 
+import { RouterProvider} from 'react-router-dom'
+import Router from './routes/routes'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={Router} />
     <Toaster />
-    <App />
   </StrictMode>,
 )
--- frontend/src/shared/pages/Page404.jsx
+++ frontend/src/shared/pages/Page404.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Page404.css'
+
+import { Link } from 'react-router-dom'
 
 const Page404 = () => {
   return (
-    <div>Page404</div>
+    <div className='page-404'>
+      <h1 className='title'>404 - Page Not Found</h1>
+      <p className='text'>The page you are looking for does not exist.</p>
+      <Link to="/" className='link'>
+        Go to Home Page
+      </Link>
+    </div>
   )
 }
 
