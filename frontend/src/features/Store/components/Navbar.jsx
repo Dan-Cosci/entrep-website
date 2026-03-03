2b014b2d3fc8eb0c9f00917831bdb0864136bd90
Daniel <allinasdaniel@gmail.com>
2026-03-03 11:58:59 +0800

    Rough store layout

--- frontend/src/features/Store/components/Navbar.jsx
+++ frontend/src/features/Store/components/Navbar.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
+
+  const [search,setSearch] = useState('')
+
   return (
-    <header className='Header'>
-      <nav className='nav'>
-        fdas
-      </nav>
-    </header>
+    <nav className='nav'>
+      <div className="logo">
+        logo
+      </div>
+
+      <div className="search">
+        <input 
+        type="text" 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder='Search'
+      />
+      </div>
+
+      <div className="link">
+        <ul>
+          <li>Home</li>
+          <li>Sale</li>
+          <li>Latest</li>
+        </ul>
+      </div>
+
+    </nav>
   )
 }
 
